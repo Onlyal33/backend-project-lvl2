299958b0a74ba33f88d82f4c3881b6e79a7cf884
onlyal33 <alex.lapygin@gmail.com>
2019-10-28 21:02:05 +0300

    stage 3 - added flat json comparison

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+import program from 'commander';
+import { version, description } from '../../package.json';
 import genDiff from '..';
 
-genDiff();
+program
+  .version(version)
+  .description(description)
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
+  .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import program from 'commander';
-import { version, description } from '../package.json';
+import _ from 'lodash';
+import fs from 'fs';
 
-export default () => {
-  program
-    .version(version)
-    .description(description)
-    .option('-f, --format [type]', 'Output format')
-    .arguments('<firstConfig> <secondConfig>')
-    .action((firstConfig, secondConfig) => {
-    })
-    .parse(process.argv);
+const getConfig = (path) => JSON.parse(fs.readFileSync(path));
+const getTextProperty = (key, value) => `${key}: ${value}\n`;
+
+export default (firstPath, secondPath) => {
+  const firstConfig = getConfig(firstPath);
+  const secondConfig = getConfig(secondPath);
+  const halfResult = Object.keys(firstConfig).reduce((acc, key) => {
+    const firstValue = firstConfig[key];
+    if (_.has(secondConfig, key)) {
+      const secondValue = secondConfig[key];
+      return firstValue === secondValue
+        ? `${acc}   ${getTextProperty(key, firstValue)}`
+        : `${acc} - ${getTextProperty(key, firstValue)} + ${getTextProperty(key, secondValue)}`;
+    }
+    return `${acc} - ${getTextProperty(key, firstValue)}`;
+  }, '{\n');
+  return `${Object.keys(secondConfig).reduce((acc, key) => {
+    const value = secondConfig[key];
+    return _.has(firstConfig, key) ? acc : `${acc} + ${getTextProperty(key, value)}`;
+  }, halfResult)}}`;
 };
